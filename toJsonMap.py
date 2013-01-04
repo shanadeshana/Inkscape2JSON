0d4ddc81895c7f826cd15dd297e871798903663f
fglesser <fglesser@fglesser-Precision-WorkStation-T3400.(none)>
2013-01-04 14:16:24 -0500

    adding an option to have the width and height

--- toJsonMap.py
+++ toJsonMap.py
@@ -14,12 +14,18 @@ class SplitIt(inkex.Effect):
 		inkex.Effect.__init__(self)
 		self.OptionParser.add_option("--key",
 						action="store",
-						dest="key", default=True,
+						dest="key", default=False,
 						help="use id as key")
+		self.OptionParser.add_option("--size",
+						action="store",
+						dest="size", default=False,
+						help="set size of object")
 
 	def getCCFromGroup(self, node):
 		minx = None
 		miny = None
+		maxx = None
+		maxy = None
 		for child in node:
 
 			attributes = child.attrib
@@ -37,11 +43,26 @@ class SplitIt(inkex.Effect):
 
 			if miny is None or y < miny:
 				miny = y
-		return {'x': minx, 'y': miny}
+
+			if maxx is None or x > maxx:
+				maxx = x
+
+			if maxy is None or y > maxy:
+				maxy = y
+
+		if self.options.size:
+			return {
+				'x': minx, 'y': miny,
+				'width': maxx - minx, 'height': maxy - miny
+			}
+		else:
+			return {'x': minx, 'y': miny}
 
 	def getCCFromPath(self, node):
 		minx = None
 		miny = None
+		maxx = None
+		maxy = None
 		paths = simplepath.parsePath(node.get('d'))
 
 		for path in paths:
@@ -51,22 +72,29 @@ class SplitIt(inkex.Effect):
 				x = path[1][-2]
 				y = path[1][-1]
 
-				if minx is None or x < minx:
-					minx = x
-
-				if miny is None or y < miny:
-					miny = y
 			else:
 				for i in range(0, len(path[1]), 2):
 					x = asInt(path[1][i])
 					y = asInt(path[1][i + 1])
 
-					if minx is None or x < minx:
-						minx = x
+			if minx is None or x < minx:
+				minx = x
 
-					if miny is None or y < miny:
-						miny = y
-		return {'x': minx, 'y': miny}
+			if miny is None or y < miny:
+				miny = y
+
+			if maxx is None or x > maxx:
+				maxx = x
+
+			if maxy is None or y > maxy:
+				maxy = y
+		if self.options.size:
+			return {
+				'x': minx, 'y': miny,
+				'width': maxx - minx, 'height': maxy - miny
+			}
+		else:
+			return {'x': minx, 'y': miny}
 
 	def getCC(self, node):
 		cc = {'x': 0, 'y': 0}
@@ -74,11 +102,14 @@ class SplitIt(inkex.Effect):
 			cc = self.getCCFromGroup(node)
 		elif node.tag == inkex.addNS('path', 'svg'):
 			cc = self.getCCFromPath(node)
-		elif not 'x' in node.attrib or not 'y' in node.attrib:
-			cc = node.attrib
 		else:
 			cc['x'] = asInt(node.get('x'))
 			cc['y'] = asInt(node.get('y'))
+
+		if self.options.size and \
+				'width' in node.attrib and 'height' in node.attrib:
+			cc['width'] = asInt(node.get('width'))
+			cc['height'] = asInt(node.get('height'))
 
 		for child in node.iter():
 			if child.tag == inkex.addNS('desc', 'svg'):
@@ -88,6 +119,9 @@ class SplitIt(inkex.Effect):
 		return cc
 
 	def effect(self):
+		self.options.size = self.options.size.lower() == 'true'
+		self.options.key = self.options.key.lower() == 'true'
+
 		nodes = None
 		if len(self.selected.keys()) > 0:
 			nodes = self.selected.values()
@@ -95,7 +129,7 @@ class SplitIt(inkex.Effect):
 			path = "./svg:g/*[@id]"
 			nodes = self.document.iterfind(path, namespaces=inkex.NSS)
 
-		if self.options.key == 'true':
+		if self.options.key:
 			elems = {}
 			for node in nodes:
 				elems[node.get('id')] = self.getCC(node)
